cf3cd1211a61e3e25d38e0bdd0458f70a902f4c6
CHAN Vonjy Tahina <51434462+vonjytahina@users.noreply.github.com>
2023-06-02 10:28:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -190,8 +190,7 @@ for (let i = 0; i < prevButton.length; i++) {
 changePlan.onclick = (e) => {
   e.preventDefault();
   stepNumber[3].classList.remove("active");
-
-stepNumber[1].classList.add("active")
+  stepNumber[1].classList.add("active")
   total = 0;
   formStep[3].style.zIndex = 0;
   formStep[1].style.zIndex = 1;
